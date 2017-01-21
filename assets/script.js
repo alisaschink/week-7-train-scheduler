f063018e32889ee1f05a11a5ca169c92a7cdc702
Alisa Schink <Alisa Schink>
2017-01-20 21:29:04 -0500

    update table bug fixed

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
  var destination = "";
  var time = "";
  var frequency = "";
- var currentTime = moment(currentTime).format("hh:mm");
+ var currentTime = moment(currentTime).format('hh:mm');
  var firstTimeConverted;
  var clock;
  var date;
@@ -42,9 +42,9 @@ $(document).ready(function() {
 
 
  // calculate time when next train arrives
- function calcTime() {
+ function showTrains() {
   //time difference = current time - time of first train
-  var timeDiff = moment().diff(firstTimeConverted, "minutes");
+  var timeDiff = moment().diff(firstTimeConverted, 'minutes');
   console.log("DIFFERENCE IN TIME: " + timeDiff);
   //timeDiff % frequency = minutesAgo
   var minutesAgo = timeDiff % trainFreq;
@@ -58,37 +58,35 @@ $(document).ready(function() {
   //format new time
   nextTrainTime = moment(nextTrain).format("hh:mm");
   console.log("ARRIVAL TIME: " + nextTrainTime);
- };
 
-
- // displays train info in table
- function showTrains() {
+  //appends train info to table
   $("#table-body").append("<tr class='table-row'><td class='table-name'> " + trainName +
-   " </td><td class='table-desination'> " + trainDes +
-   " </td><td class='table-frequency'> " + trainFreq + " </td><td class='next-train'> " + nextTrainTime +
-   " </td><td class='minutes-away'> " + minutesLeft +
-   " </td></tr>");
+  " </td><td class='table-desination'> " + trainDes +
+  " </td><td class='table-frequency'> " + trainFreq + " </td><td class='next-train'> " + nextTrainTime +
+  " </td><td class='minutes-away'> " + minutesLeft +
+  " </td></tr>");
  };
+
 
  // updates table with train info
  function updateTable() {
-  // empty tables
+  // empty table
   $("#table-body").empty();
 
   // updates values in table for each object key
   for (i = 0; i < trainNames.length; i++) {
+    //ensures that time is in the past
    firstTimeConverted = moment(trainTimes[i], "hh:mm").subtract(1, "years");
    trainName = trainNames[i]
    trainDes = trainDests[i];
    trainFreq = trainFreqs[i];
-   calcTime();
    showTrains();
 
   };
  };
 
  // capture button click
- $("#add-train").on("click", function(event) {
+ $(document).on('click', '#add-train', function(event) {
   event.preventDefault();
   name = $("#name-input").val().trim();
   destination = $("#destination-input").val().trim();
@@ -109,35 +107,16 @@ $(document).ready(function() {
  // event listener triggers if a child is added 
  dataRef.ref().on("child_added", function(childSnapshot) {
 
-  trainName = childSnapshot.val().name
-  trainDes = childSnapshot.val().destination
-  trainFreq = childSnapshot.val().frequency
-   // First Time (pushed back 1 year to make sure it comes before current time)
-  firstTimeConverted = moment(childSnapshot.val().time, "hh:mm").subtract(1, "years");
-  calcTime();
-  showTrains();
+  //pushes new values to arrays
+  trainNames.push(childSnapshot.val().name);
+  trainDests.push(childSnapshot.val().destination);
+  trainTimes.push(childSnapshot.val().time);
+  trainFreqs.push(childSnapshot.val().frequency);
+
+  updateTable();
 
 
   // handle the errors
- }, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
- });
-
-
- //pulls data out of firebase and updates table
- dataRef.ref().on("value", function(snapshot) {
-  var sv = snapshot.val();
-  var sv_arr = Object.keys(sv);
-
-  for (i = 0; i < sv_arr.length; i++) {
-   trainNames.push(sv[sv_arr[i]].name);
-   trainDests.push(sv[sv_arr[i]].destination);
-   trainTimes.push(sv[sv_arr[i]].time);
-   trainFreqs.push(sv[sv_arr[i]].frequency);
-  };
-
-  updateTable();
-  // Handle the errors
  }, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
  });
